c900fa872d42bb5c204ed3ca7a9c4cef74104f81
bananvyhe <loadonden@yahoo.com>
2018-05-20 10:00:45 +0500

    pre deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,12 +56,14 @@ config.webpacker.check_yarn_integrity = false
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "testapp_#{Rails.env}"
   config.action_mailer.perform_caching = false
-config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'pixeltech.ru' }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
